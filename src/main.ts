dbead6d8ade5a244a8f7026ae6e3cb780b1888be
[zedic] <oluwaseyifunmitan1@gmail.com>
2025-04-07 13:00:46 +0100

    block data structure

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+
+import { Block } from '../lib/main'
+
+const ud = [
+    { id: 1, name: "john" },
+    {id: 2, name: "doe"},
+    {id: 3, name: "sam"}
+]
+
+const users = new Block([])
+console.log(users)
+//add, remove, remove from index, map, filter, blocks--
